fca4160b3bd648b083d49033af15c24f55ac08c2
Phathdt <phathdt379@gmail.com>
2021-02-12 21:05:36 +0700

    update file ex

--- apps/shin_client/lib/shin_client.ex
+++ apps/shin_client/lib/shin_client.ex
@@ -6,4 +6,8 @@ defmodule ShinClient do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  def hello do
+    "Hello World"
+  end
 end

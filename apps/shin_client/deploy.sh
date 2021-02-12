63b92231909ae05b356df8e88dbb42811c9412d6
Phathdt <phathdt379@gmail.com>
2021-02-12 17:35:22 +0700

    update port

--- apps/shin_client/deploy.sh
+++ apps/shin_client/deploy.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-
-echo ${DOCKER_IMAGE}$APP:$TAG
-
-# docker rm -f shin-client
+docker rm -f shin-client
 
 docker run -d --name shin-client \
   --network my-net \
   -e DATABASE_URL=postgres://phathdt379:password123@10.148.0.13:5432/shin \
   -e SECRET_KEY_BASE=43HToczwEtbLIujVyC/L7syuUDAEHklOVydWoxt6Tu33IfUGT8ZaQbpVTcH5jnw8 \
-  -p 6000:4000 \
+  -e PORT=6000 \
+  -p 6000:6000 \
   ${DOCKER_IMAGE}$APP:$TAG

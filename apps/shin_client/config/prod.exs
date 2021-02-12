c866918811f0575d56bc5463738ae7a719a0858f
Phathdt <phathdt379@gmail.com>
2021-02-12 18:13:08 +0700

    update code config prod

--- apps/shin_client/config/prod.exs
+++ apps/shin_client/config/prod.exs
@@ -1,8 +1,6 @@
 use Mix.Config
 
 config :shin_client, ShinClientWeb.Endpoint,
-  url: [host: "example.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production

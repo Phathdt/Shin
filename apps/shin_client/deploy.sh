d743b4280c11547eab337a22d159d26749a73b03
Phathdt <phathdt379@gmail.com>
2021-02-12 17:30:10 +0700

    update deploy

--- apps/shin_client/deploy.sh
+++ apps/shin_client/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-docker rm -f shin_client
 
-docker run -d --name shin_client \
+echo ${DOCKER_IMAGE}:$TAG
+
+# docker rm -f shin-client
+
+docker run -d --name shin-client \
   --network my-net \
   -e DATABASE_URL=postgres://phathdt379:password123@10.148.0.13:5432/shin \
   -e SECRET_KEY_BASE=43HToczwEtbLIujVyC/L7syuUDAEHklOVydWoxt6Tu33IfUGT8ZaQbpVTcH5jnw8 \
-  -p 8802:4000 \
+  -p 6000:4000 \
   ${DOCKER_IMAGE}:$TAG

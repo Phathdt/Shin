5648ee299c4f065f0c4f7e4579d0b405f94ca36e
Phathdt <phathdt379@gmail.com>
2021-02-12 18:31:27 +0700

    update port

--- apps/shin_admin/config/prod.exs
+++ apps/shin_admin/config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :shin_admin, ShinAdminWeb.Endpoint,
-  url: [host: "35.213.174.112", port: 80],
+  url: [host: "35.213.174.112", port: 7000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- apps/shin_client/config/prod.exs
+++ apps/shin_client/config/prod.exs
@@ -1,7 +1,7 @@
 use Mix.Config
 
 config :shin_client, ShinClientWeb.Endpoint,
-  url: [host: "35.213.174.112", port: 80],
+  url: [host: "35.213.174.112", port: 6000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
